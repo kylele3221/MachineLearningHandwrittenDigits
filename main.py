345b49e9e0cbd4d30c439736f20f0a1519dc66b9
Luke Rasmussen <lukerasmussen@MacBook-Pro-7.local>
2024-10-13 21:22:03 -0700

    added data set

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 import tensorflow as tf
+import matplotlib.pyplot as plt
+
+
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+for i in range(5):
+    plt.imshow(x_train[i])
+    plt.show()
+
+
