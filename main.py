0fb4bc2a1f1d56273afde662eb3005e9864083af
kylele3221 <kylele3221@gmail.com>
2024-10-15 12:40:06 -0700

    Added comments

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 
 
+# Get mnist dataset
 mnist = tf.keras.datasets.mnist
 
+# Splits the mnist dataset into two datatsets.  One for training and one for testing 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
+# Shows the first five images in the training set
 for i in range(5):
     plt.imshow(x_train[i])
     plt.show()
